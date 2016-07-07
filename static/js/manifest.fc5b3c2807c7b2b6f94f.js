9b75dfcebd062db1f752f0c0c3e2a592f8f588f0
Miljan Aleksic <miljan@aleksic.es>
2016-07-07 18:26:50 +0700

    update docs

---
+++ static/js/manifest.fc5b3c2807c7b2b6f94f.js
@@ -0,0 +1,7 @@
+/*!
+ * Vuikit v0.3.0 (https://github.com/vuikit/vuikit)
+ * (c) 2016 ZOOlanders
+ * Released under the MIT License.
+ */
+!function(e){function t(a){if(n[a])return n[a].exports;var r=n[a]={exports:{},id:a,loaded:!1};return e[a].call(r.exports,r,r.exports,t),r.loaded=!0,r.exports}var a=window.webpackJsonp;window.webpackJsonp=function(o,c){for(var p,s,l=0,i=[];l<o.length;l++)s=o[l],r[s]&&i.push.apply(i,r[s]),r[s]=0;for(p in c)e[p]=c[p];for(a&&a(o,c);i.length;)i.shift().call(null,t);return c[0]?(n[0]=0,t(0)):void 0};var n={},r={0:0};t.e=function(e,a){if(0===r[e])return a.call(null,t);if(void 0!==r[e])r[e].push(a);else{r[e]=[a];var n=document.getElementsByTagName("head")[0],o=document.createElement("script");o.type="text/javascript",o.charset="utf-8",o.async=!0,o.src=t.p+"static/js/"+e+"."+{1:"968ef84a1fd2b3a1eba5",2:"e8199012ab59d63dfbcb"}[e]+".js",n.appendChild(o)}},t.m=e,t.c=n,t.p=""}([]);
+//# sourceMappingURL=manifest.fc5b3c2807c7b2b6f94f.js.map
